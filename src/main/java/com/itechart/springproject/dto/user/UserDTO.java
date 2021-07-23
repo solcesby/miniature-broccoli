c8fd58ae004116921437590f91dfbb39bacbc5bb
Mikhail.Kniga <solcesby@gmail.com>
2021-07-23 13:12:45 +0300

    minor changes

--- src/main/java/com/itechart/springproject/dto/user/UserDTO.java
+++ src/main/java/com/itechart/springproject/dto/user/UserDTO.java
@@ -3,17 +3,13 @@ package com.itechart.springproject.dto.user;
 import com.itechart.springproject.dto.user.enums.UserRoleDTO;
 import lombok.Data;
 
-import java.util.UUID;
-
 @Data
 public class UserDTO {
 
-    private UUID id;
     private UserRoleDTO role;
     private String firstName;
     private String lastName;
     private String phone;
     private String email;
-//    private String password;
 
 }
