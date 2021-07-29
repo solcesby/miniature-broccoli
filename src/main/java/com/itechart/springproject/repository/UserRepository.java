ec74a899b12ca27626e45f3ca48a7f40cce033c9
Mikhail.Kniga <solcesby@gmail.com>
2021-07-29 18:37:32 +0300

    minor changes

--- src/main/java/com/itechart/springproject/repository/UserRepository.java
+++ src/main/java/com/itechart/springproject/repository/UserRepository.java
@@ -4,14 +4,22 @@ import com.itechart.springproject.entity.user.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
-    Optional<UserEntity> findById(UUID id);
 
-    Page<UserEntity> findAllByCreatedAtIsNull(Pageable pageable);
+    @Query(
+            value = "SELECT * FROM spring.public.users " +
+                    "WHERE deleted_at IS NULL",
+            nativeQuery = true
+    )
+    Page<UserEntity> findAllByDeletedAtIsNull(Pageable pageable);
 
-    Page<UserEntity> findAll(Pageable pageable);
+    boolean existsByEmail(String email);
+
+
 }
