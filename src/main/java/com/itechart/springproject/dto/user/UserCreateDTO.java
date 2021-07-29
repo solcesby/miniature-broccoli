4fad636e8c7f8e1c2affa9c1745c49006fa9a6b7
Mikhail.Kniga <solcesby@gmail.com>
2021-07-29 18:22:59 +0300

    add validation annotations

--- src/main/java/com/itechart/springproject/dto/user/UserCreateDTO.java
+++ src/main/java/com/itechart/springproject/dto/user/UserCreateDTO.java
@@ -1,11 +1,22 @@
 package com.itechart.springproject.dto.user;
 
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
 public class UserCreateDTO {
 
     private String firstName;
+
     private String lastName;
+
     private String phone;
+
+    @NotEmpty(message = "email can't be empty")
     private String email;
+
+    @NotEmpty(message = "password can't be empty")
     private String password;
 
 }
