bbb8000f6fb480bc0fe3d861129b0d81a0aa5eaa
Mikhail.Kniga <solcesby@gmail.com>
2021-07-29 18:24:12 +0300

    minor changes

--- src/main/java/com/itechart/springproject/mapper/UserMapper.java
+++ src/main/java/com/itechart/springproject/mapper/UserMapper.java
@@ -17,9 +17,11 @@ public interface UserMapper {
     UserDTO toDTO(UserEntity user);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "role", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     @Mapping(target = "deletedAt", ignore = true)
+    @Mapping(target = "recipientEmails", ignore = true)
     UserEntity toEntity(UserCreateDTO userCreateDTO);
 
 }
