d2bffcc122c3c00936bff7bc5925c1b32326b395
Mikhail.Kniga <solcesby@gmail.com>
2021-08-05 16:04:01 +0300

    minor changes

--- src/main/java/com/itechart/springproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/itechart/springproject/service/impl/UserServiceImpl.java
@@ -4,9 +4,10 @@ import com.itechart.springproject.dto.user.UserCreateDTO;
 import com.itechart.springproject.dto.user.UserDTO;
 import com.itechart.springproject.dto.user.UserUpdateDTO;
 import com.itechart.springproject.entity.user.UserEntity;
+import com.itechart.springproject.entity.user.UserRoleEntity;
 import com.itechart.springproject.repository.UserRepository;
 import com.itechart.springproject.service.UserService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -18,24 +19,27 @@ import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
 import java.util.UUID;
 
-import static com.itechart.springproject.entity.user.enums.UserRoleEntity.ROLE_USER;
+import static com.itechart.springproject.entity.user.enums.RoleEntity.ROLE_USER;
 import static com.itechart.springproject.mapper.UserMapper.USER_MAPPER;
 import static java.lang.String.format;
 import static java.time.LocalDateTime.now;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
     private final BCryptPasswordEncoder passwordEncoder;
 
     @Override
     @Transactional
     public UserDTO create(final UserCreateDTO userCreateDTO) {
         final UserEntity user = USER_MAPPER.toEntity(userCreateDTO);
-        user.setRole(ROLE_USER);
+        user.setFirstName(userCreateDTO.getFirstName());
+        user.setLastName(userCreateDTO.getLastName());
+        user.setPhone(userCreateDTO.getPhone());
+        user.setEmail(userCreateDTO.getEmail());
+        user.setRole(generateDefaultRole(user));
         user.setPassword(passwordEncoder.encode(userCreateDTO.getPassword()));
         user.setCreatedAt(now());
 
@@ -53,6 +57,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Page<UserDTO> findAllActive(Integer page, Integer size) {
         final Pageable pageable = PageRequest.of(page, size);
         return userRepository.findAllByDeletedAtIsNull(pageable).map(USER_MAPPER::toDTO);
@@ -73,9 +78,9 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(() ->
                         new EntityNotFoundException(format("User with id: %s not found", userUpdateDTO.getId())));
         user.setFirstName(userUpdateDTO.getFirstName());
-        user.setFirstName(userUpdateDTO.getLastName());
-        user.setFirstName(userUpdateDTO.getPhone());
-        user.setFirstName(userUpdateDTO.getPassword());
+        user.setLastName(userUpdateDTO.getLastName());
+        user.setPhone(userUpdateDTO.getPhone());
+        user.setPassword(passwordEncoder.encode(userUpdateDTO.getPassword()));
         user.setUpdatedAt(now());
 
         final UserEntity updated = userRepository.save(user);
@@ -97,5 +102,12 @@ public class UserServiceImpl implements UserService {
         if (userRepository.existsByEmail(email)) {
             throw new EntityExistsException(format("Email: %s already exists", email));
         }
+    }
+
+    private UserRoleEntity generateDefaultRole(UserEntity user) {
+        final var role = new UserRoleEntity();
+        role.setRole(ROLE_USER);
+        role.setUser(user);
+        return role;
     }
 }
